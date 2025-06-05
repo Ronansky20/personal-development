80ea7e2c2a4c16f9ac7e22ac8e145d43ad6290af
ronansky20 <ronandrost20@gmail.com>
2025-06-06 01:39:49 +0200

    league-api-addition

---
+++ league-api/Program.cs
@@ -0,0 +1,44 @@
+﻿using DotNetEnv;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Env.Load();
+
+        string? apiKey = Environment.GetEnvironmentVariable("RIOT_API_KEY");
+        Console.WriteLine(apiKey);
+
+        if (string.IsNullOrWhiteSpace(apiKey))
+        {
+            Console.WriteLine("No API Key");
+            return;
+        }
+
+        string region = "https://europe.api.riotgames.com";
+
+        Console.WriteLine("Enter User Name");
+        string? gameName = Console.ReadLine();
+        Console.WriteLine("Enter Tag");
+        string? userTag = Console.ReadLine();
+
+        using HttpClient client = new HttpClient();
+
+        try
+        {
+            string requestUrl = $"{region}/riot/account/v1/accounts/by-riot-id/{gameName}/{userTag}";
+            client.DefaultRequestHeaders.Add("X-Riot-Token", apiKey);
+
+            HttpResponseMessage response = await client.GetAsync(requestUrl);
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Response:");
+            Console.WriteLine(responseBody);
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"Request Error: {e.Message}");
+        }
+    }
+}
